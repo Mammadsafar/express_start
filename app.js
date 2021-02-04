fb93e9ea0bd9c58c711b6b6418f5083f721ddbba
mohammd safarzadeh <mohammadsafar2014@gmail.com>
2021-02-04 12:06:03 +0330

    clean node_modules

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res){
+    console.log(req);
+})
+
+
+app.listen(5005, () => { 
+    console.log("server started on port 5005 !!");
+})
